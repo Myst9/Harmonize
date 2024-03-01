efd996660a173843644e1f93355f0153ef0744ef
CS21B043 <110807261+CS21B043@users.noreply.github.com>
2024-03-01 23:01:21 +0530

    Working API(I guess)
    Flask from BirdID website code was merged with preprocessing code given by Varshitha. Finally, we got this and we tested it by giving POST requests through Microsoft Edge Network Console.

--- test.py
+++ test.py
@@ -1,47 +1,84 @@
 from transformers import BertTokenizer
 from keras.preprocessing.sequence import pad_sequences
 
-tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
-
-new_sentence = """You must be a big fool"""
-tokenized_sentence = tokenizer.tokenize(new_sentence)
-
-MAX_LEN = 128
-
-# Use the BERT Tokenizer to convert the tokens to their index numbers in the BERT vocabulary
-input_ids = tokenizer.convert_tokens_to_ids(tokenized_sentence)
-
-# Pad the input tokens
-padded_input = pad_sequences([input_ids], maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
-
-# Create attention mask
-attention_mask = [float(i > 0) for i in padded_input[0]]
+from flask import Flask, request, render_template, jsonify
+from flask_cors import CORS
+from gevent.pywsgi import WSGIServer
 
 import torch
+import numpy as np
 
-input_tensor = torch.tensor(padded_input)
-attention_mask_tensor = torch.tensor([attention_mask])
+app = Flask(__name__)
+CORS(app)
 
+###MODEL AND TOKENIZER LOADING###
 from transformers import BertForSequenceClassification
 
 # Load the pre-trained model
 model = BertForSequenceClassification.from_pretrained('bert-base-uncased', num_labels=2)
-
+print("m loading")
 # Load the saved model state
 model.load_state_dict(torch.load('model2.pth', map_location=torch.device('cpu')))
+print("model loaded")
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased', do_lower_case=True)
 
 # Set the model to evaluation mode
 model.eval()
+print("set to eval mode")
+######################
 
-# Make predictions
-with torch.no_grad():
-    outputs = model(input_tensor, attention_mask=attention_mask_tensor)
 
-# Get the predicted class probabilities
-predicted_probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+def model_predict_dsh(sentence):
+    tokenized_sentence = tokenizer.tokenize(sentence)
 
-# Get the predicted class index
-predicted_class = torch.argmax(predicted_probs, dim=1).item()
 
-print("Predicted Class:", predicted_class)
-print("Predicted Probabilities:", predicted_probs.numpy())
+    MAX_LEN = 128
+
+    # Use the BERT Tokenizer to convert the tokens to their index numbers in the BERT vocabulary
+    input_ids = tokenizer.convert_tokens_to_ids(tokenized_sentence)
+
+    # Pad the input tokens
+    padded_input = pad_sequences([input_ids], maxlen=MAX_LEN, dtype="long", truncating="post", padding="post")
+
+    # Create attention mask
+    attention_mask = [float(i > 0) for i in padded_input[0]]
+
+
+
+    input_tensor = torch.tensor(padded_input)
+    attention_mask_tensor = torch.tensor([attention_mask])
+
+
+    # Make predictions
+    with torch.no_grad():
+        outputs = model(input_tensor, attention_mask=attention_mask_tensor)
+
+    # Get the predicted class probabilities
+    predicted_probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+    # Get the predicted class index
+    predicted_class = torch.argmax(predicted_probs, dim=1).item()
+    print(predicted_class, predicted_probs.numpy())
+    return (predicted_class,predicted_probs.numpy())
+
+
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    if request.method == 'POST':
+        # Get the image from post request
+        # img = base64_to_pil(request.json)
+        
+        # Make prediction
+        print("hi")
+        prediction = model_predict_dsh(request.json)
+        pred = prediction[1]
+        result = prediction[0]
+        pred_probability = "{:.3f}".format(np.amax(pred)) 
+        
+        return jsonify(result=result, probability=pred_probability)
+
+    return None
+
+
+if __name__ == '__main__':
+    http_server = WSGIServer(('127.0.0.1', 5000), app)
+    http_server.serve_forever()
